5040ce17babf06d5fb6995daf0add9a7f82ac0d7
real2two <26689496+real2two@users.noreply.github.com>
2024-06-23 13:00:59 -0400

    Removed useless comments

--- src/server.ts
+++ src/server.ts
@@ -11,31 +11,8 @@ app.get('/', (c) => {
 app.post('/', async (c) => {
   const { searchParams } = new URL(c.req.raw.url);
   const publicKey = searchParams.get('public_key') || '';
-  // const token = searchParams.get('token') || '';
-  // const commandName = searchParams.get('command') || 'c';
 
   if (!/^[a-z0-9]{64}$/.test(publicKey)) return c.text('Invalid public key.', 400);
-  // if (!/^[-_\p{L}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$/u.test(commandName)) {
-  // return c.text('Invalid command name.', 400);
-  // }
-  // if (!/[\w-]{84}|[\w-]{24}\.[\w-]{6}\.[\w-]{27}/.test(token)) return c.text('Invalid token.', 403);
-
-  // const command = {
-  //   name: commandName,
-  //   description: 'Send a message with this profile',
-  //   options: [
-  //     {
-  //       type: 3,
-  //       name: '_',
-  //       description: 'This is the message to send',
-  //       required: true,
-  //       min_length: 1,
-  //       max_length: 4000,
-  //     },
-  //   ],
-  //   integration_types: [0, 1],
-  //   contexts: [0, 1, 2],
-  // };
 
   const { isValid, interaction } = await server.verifyDiscordRequest(c.req, publicKey);
   if (!isValid || !interaction) return c.text('Bad request signature.', 401);
@@ -43,24 +20,10 @@ app.post('/', async (c) => {
   const payload = {
     type: interaction?.type,
     applicationId: interaction?.application_id,
-    // commandName: interaction?.data?.name?.toLowerCase(),
     content: interaction?.data?.options?.[0]?.value,
   };
 
   if (payload.type === 1) {
-    // const createCommandReq = await fetch(
-    //   `https://discord.com/api/v10/applications/${encodeURIComponent(payload.applicationId)}/commands`,
-    //   {
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       Authorization: `Bot ${token}`,
-    //     },
-    //     method: 'PUT',
-    //     body: JSON.stringify([command]),
-    //   },
-    // );
-    // if (createCommandReq.status !== 200) return c.text('Failed to create commands', 403);
-
     return c.json({ type: 1 });
   }
 
@@ -68,9 +31,6 @@ app.post('/', async (c) => {
   if (!/^(\d{17,21})$/.test(payload.applicationId)) {
     return c.text('Invalid interaction.application_id.', 401);
   }
-  // if (!/^[-_\p{L}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$/u.test(payload.commandName)) {
-  //   return c.text('Invalid interaction.data.name.', 400);
-  // }
   if (
     typeof payload.content !== 'string' ||
     payload.content.length < 1 ||
@@ -79,14 +39,12 @@ app.post('/', async (c) => {
     return c.text('Invalid interaction.data.options[0].value.', 400);
   }
 
-  // if (payload.commandName !== commandName.toLowerCase())
-  //   return c.json({ error: 'Unknown Type' }, 400);
-
   return c.json({
     type: 4,
     data: { content: payload.content },
   });
 });
+
 app.all('*', (c) => c.text('Not Found.', 404));
 
 async function verifyDiscordRequest(req: HonoRequest<'/'>, publicKey: string) {
